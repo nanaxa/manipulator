c0bc3dabf350d66d484597ab706ac82d947d01eb
nanaxa <pavpildan@gmail.com>
2021-07-06 23:12:41 +0700

    add react app, add scss, add header, add header adaptive for mobile

---
+++ src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./style.scss";
+
+export function Header() {
+  return <header className="header">
+
+              <section className="header__content">
+
+                  <h1 className="header__content__heading">Заказать манипулятор в Томске</h1>
+                  <a className="header__content__tel" href="tel:+1234567890">+1234567890</a>
+
+              </section>
+
+        </header>
+
+}
