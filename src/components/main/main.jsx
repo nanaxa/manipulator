593d54b384a8ba0b6ad28d579558ede44289877a
nanaxa <pavpildan@gmail.com>
2021-07-15 13:25:35 +0700

    add option component

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import "./main.scss"
 import {Description} from "./description/description.jsx" ;
+import {Option} from "./Options/Options";
 
 export const Main = () => {
     return <div className="main">
         <Description />
+        <Option />
     </div>
 }
